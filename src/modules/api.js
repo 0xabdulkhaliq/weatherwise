3418a7c3bb53b09e94735e785e8b62b93fc104f9
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2023-09-03 08:38:33 +0530

    chore: update backend uri

--- src/modules/api.js
+++ src/modules/api.js
@@ -3,7 +3,7 @@ import { instantiateUIChanges, instantiate404Prompt } from "./ui"
 async function getWeatherData(latitude, longitude) {
   try {
     const response = await fetch(
-      `http://localhost:7417/weather?latitude=${latitude}&longitude=${longitude}` // Backend, to secure API Key
+      `https://weatherwise-backend.vercel.app/weather?latitude=${latitude}&longitude=${longitude}` // Backend
     )
 
     if (response.ok) {
